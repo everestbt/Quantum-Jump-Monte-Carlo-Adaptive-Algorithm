376331fe314dc31a6341622ea83d741f0c2dd560
Ben <everest.bt@gmail.com>
2017-06-13 15:14:08 +0100

    Added a function to evolve by a particular exponent to unify code.

--- QJMCEvolve.py
+++ QJMCEvolve.py
@@ -1,8 +1,12 @@
 import QJMCSetUp
 
-#TODO use this where possible in the low memory solution
 def evolvePsi(psi, t, dt, HEff):
     HEffExponentDt = QJMCSetUp.HEffExponentProduction(HEff,dt)
+    psi = HEffExponentDt.dot(psi)
+    t += dt
+    return t, psi
+
+def evolvePsiByExponent(psi, t, dt, HEffExponentDt):
     psi = HEffExponentDt.dot(psi)
     t += dt
     return t, psi
